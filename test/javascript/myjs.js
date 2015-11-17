9af49f3329a3ba45eef4e16fb1dc304672d578d1
Teu Raggi <teresa.raggi@gmail.com>
2015-11-17 02:24:58 -0300

    Valida el Form...

---
+++ test/javascript/myjs.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+	    $( "#submitForm" ).on( "click", function() {
+        $( "#cotizaForm" ).validate( 
+
+        	{
+		    wrapper: 'span',
+		    errorPlacement: function (error, element) {
+		        error.css({'padding-left':'10px','margin-right':'20px','padding-bottom':'2px'});
+		        error.addClass("arrow")
+		        error.insertAfter(element);
+		    }
+		    
+        	);
+    });
+
+});
